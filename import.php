6dc911bbbf9a99e134db21a0c1745aa07464b92c
David Rosales <davidrosales270@gmail.com>
2017-05-26 15:23:16 -0600

    Update import.php

--- import.php
+++ import.php
@@ -7,79 +7,76 @@
  */
 include ('simple_html_dom.php');
 
-$base = 'https://www.lbcomponents.com/esd-suppressors.html';
 
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-curl_setopt($curl, CURLOPT_HEADER, false);
-curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($curl, CURLOPT_URL, $base);
-curl_setopt($curl, CURLOPT_REFERER, $base);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-$str = curl_exec($curl);
-curl_close($curl);
-
-
-// Create a DOM object
-$html_base = new simple_html_dom();
-// Load HTML from a string
-$html_base->load($str);
 
 include_once 'database.php';
 $database = new Database();
 $db = $database->getConnection();
 
-// instantiate product object
 include_once 'product.php';
 
 
+$from = 3651;
+$to = 3700;
 
-// get posted data
+for($x= $from; $x <= $to; $x++){
 
 
 
-//get all category links
-foreach($html_base->find('tr') as $row) {
+    $base = 'https://lbcomponents.com/page/'.$x.'/?s';
 
-    $product = new Product($db);
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($curl, CURLOPT_URL, $base);
+    curl_setopt($curl, CURLOPT_REFERER, $base);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+    $str = curl_exec($curl);
+    curl_close($curl);
 
-    $product_name = "";
-    $manufacturer = "";
-    $product_no = "";
-    $i = 0;
-    foreach($row->find('td') as $cell) {
-        // push the cell's text to the array
 
-        if($i==0){
-            $product_no = $cell->plaintext;
-        }elseif($i==1){
-            $manufacturer = $cell->plaintext;
-        }elseif($i==2)
-        {
-            $product_name = $cell->plaintext;
+    // Create a DOM object
+        $html_base = new simple_html_dom();
+    // Load HTML from a string
+        $html_base->load($str);
+
+
+
+    foreach($html_base->find('tr') as $row) {
+
+        $product = new Product($db);
+
+        $description = "";
+        $manufacturer = "";
+        $product_no = "";
+        $i = 0;
+        foreach($row->find('td') as $cell) {
+            // push the cell's text to the array
+
+            if($i==0){
+                $product_no = $cell->plaintext;
+            }elseif($i==1){
+                $manufacturer = $cell->plaintext;
+            }elseif($i==2)
+            {
+                $description = $cell->plaintext;
+            }
+
+
+            $i++;
         }
 
+        $product->part_no = $product_no;
+        $product->manufacturer = $manufacturer;
+        $product->description = $description;
 
-        $i++;
+
+        if($product->part_no == "")
+            continue;
+
+        $product->create();
     }
-
-
-    $product->name = $product_name;
-    $product->part_no = $product_no;
-    $product->manufacturer = $manufacturer;
-    $product->created = date('Y-m-d H:i:s');
-
-    // create the product
-    if($product->create()){
-        echo "Product was created.";
-    }
-
-    // if unable to create the product, tell the user
-    else{
-        echo "Unable to create product.";
-    }
-
-
 
 }
 
